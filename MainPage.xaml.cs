dbc7dd7afc4906b72e15d2ffc371ede5cdefd23c
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-11-24 17:38:59 +0100

    Aggiungere i file di progetto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiAppScrollViewProblemNet8
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+
+}
